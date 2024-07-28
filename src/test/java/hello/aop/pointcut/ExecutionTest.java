381914737d3d2027bc4d6901c184a1b427b16b1c
Sumsan38 <ndanl4647@gmail.com>
2024-07-29 08:41:38 +0900

    aop - 타입매칭

--- src/test/java/hello/aop/pointcut/ExecutionTest.java
+++ src/test/java/hello/aop/pointcut/ExecutionTest.java
@@ -28,19 +28,20 @@ public class ExecutionTest {
     }
 
     /**
-     * 이 아래부터 execution으로 시작하는 포인트컷 표현식은 이 메서드 정보를 매칭해서 포인트컷 대상을 찾아낸다.
+     * Execution 관련 <br/>
+     * 이 아래부터 execution으로 시작하는 포인트컷 표현식은 이 메서드 정보를 매칭해서 포인트컷 대상을 찾아낸다. <br/>
      * execution(modifiers-pattern?
      *          ret-type-pattern
      *          declaring-type-pattern?name-pattern(param-pattern)
      *          throws-pattern?)
-     * execution(접근제어자? 반환타입 선언타입?메서드이름(파라미터) 예외?)
+     * execution(접근제어자? 반환타입 선언타입?메서드이름(파라미터) 예외?) <br/>
      *      메소드 실행 조인 포인트를 매칭한다.
      *      ?는 생략할 수 있다.
      *      * 같은 패턴을 지정할 수 있다
      */
 
     /*
-    1. 가장 정확한 포인트 컷
+    가장 정확한 포인트 컷
     MemberServiceImpl.hello(String) 메서드와 가장 정확하게 모든 내용이 매칭되는 표현식
      */
     @Test
@@ -57,7 +58,7 @@ public class ExecutionTest {
     }
 
     /*
-    2. 가장 많이 생략한 포인트컷
+    가장 많이 생략한 포인트컷
      */
     @Test
     void allMatch(){
@@ -72,7 +73,7 @@ public class ExecutionTest {
     }
 
     /*
-    3. 메서드 이름 매칭 관련 포인트컷
+    메서드 이름 매칭 관련 포인트컷
      */
     @Test
     void nameMatch(){
@@ -91,7 +92,7 @@ public class ExecutionTest {
     }
 
     /*
-    4. 패키지 매칭 관련 포인트컷
+    패키지 매칭 관련 포인트컷
      */
     @Test
     void packageExactMatch1(){
@@ -124,5 +125,41 @@ public class ExecutionTest {
         pointcut.setExpression("execution(* hello.aop..*.*(..))");
         assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
     }
+
+    /*
+    타입 매칭 - 부모 타입 허용
+     */
+    @Test
+    void typeExactMatch(){
+        pointcut.setExpression("execution(* hello.aop.member.MemberServiceImpl.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+    @Test
+    void typeMatchSuperType(){
+        pointcut.setExpression("execution(* hello.aop.member.MemberService.*(..))");
+        assertThat(pointcut.matches(helloMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    /*
+    타입 매칭 - 부모 타입에 있는 메서드만 허용
+     */
+    @Test
+    void typeMatchInternal() throws NoSuchMethodException{
+        pointcut.setExpression("execution(* hello.aop.member.MemberServiceImpl.*(..))");
+        Method internalMethod = MemberServiceImpl.class.getMethod("internal", String.class);
+        assertThat(pointcut.matches(internalMethod, MemberServiceImpl.class)).isTrue();
+    }
+
+    @Test
+    void typeMatchNoSuperTypeMethodFalse() throws NoSuchMethodException {
+        pointcut.setExpression("execution(* hello.aop.member.MemberService.*(..))");
+        Method internalMethod = MemberServiceImpl.class.getMethod("internal", String.class);
+        assertThat(pointcut.matches(internalMethod, MemberServiceImpl.class)).isFalse();
+    }
+
+    /*
+    파라미턴 매칭
+     */
+
 
 }
