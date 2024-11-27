1504268932c64f2f7bf1a300d359f3425b3215bc
Sumsan38 <ndanl4647@gmail.com>
2024-11-27 22:01:04 +0900

    MDC 1차 적용

--- src/main/java/com/my/bob/config/WebConfig.java
+++ src/main/java/com/my/bob/config/WebConfig.java
@@ -1,7 +1,7 @@
 package com.my.bob.config;
 
 import com.my.bob.interceptor.CustomerInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.my.bob.interceptor.MDCInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,13 +9,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Autowired
-    private CustomerInterceptor customerInterceptor;
+    private final CustomerInterceptor customerInterceptor;
+
+    private final MDCInterceptor mdcInterceptor;
+
+    public WebConfig(CustomerInterceptor customerInterceptor,
+                     MDCInterceptor mdcInterceptor) {
+        this.customerInterceptor = customerInterceptor;
+        this.mdcInterceptor = mdcInterceptor;
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 인터셉터를 등록하는 메서드
         // CustomInterceptor를 등록하고, 모든 URL에 대해 인터셉터를 적용하도록 설정
+        registry.addInterceptor(mdcInterceptor);
         registry.addInterceptor(customerInterceptor).addPathPatterns("/**");
         WebMvcConfigurer.super.addInterceptors(registry);
     }
