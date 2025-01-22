6e4c944951ded721e5adbb7ceac1d1c4147b6564
Sumsan38 <ndanl4647@gmail.com>
2025-01-22 09:04:18 +0900

    S3 기본 설정 추가
    S3를 사용하기 위해 의존성을 추가하고 yml, Configration 파일을 작성 했습니다. 그에 따라 S3Client를 사용할 수 있습니다.

---
+++ src/main/java/com/my/bob/core/external/s3/service/S3Service.java
@@ -0,0 +1,18 @@
+package com.my.bob.core.external.s3.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+
+    private final S3Client s3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+
+}
