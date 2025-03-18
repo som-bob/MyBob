b06cbf79de04b1ed3cb9b74a6afb2d5cc50d1497
Sumsan38 <ndanl4647@gmail.com>
2025-03-18 09:39:41 +0900

    RedisTemplate 작성
    - key, value 모두 String 값으로 주고 받기 위한 RedisTemplate 작성

--- src/main/java/com/my/bob/core/config/RedisConfig.java
+++ src/main/java/com/my/bob/core/config/RedisConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class RedisConfig {
@@ -18,5 +20,11 @@ public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        // key, value 모두 String 사용
+        return new StringRedisTemplate(redisConnectionFactory());
     }
 }
