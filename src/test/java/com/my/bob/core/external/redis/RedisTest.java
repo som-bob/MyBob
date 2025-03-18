0c68164afd8332f8ace9c318371de9ef518266ef
Sumsan38 <ndanl4647@gmail.com>
2025-03-18 09:06:10 +0900

    Redis Config 및 테스트 코드 작성
    - RedisConfig를 작성 했습니다.
    - Redis의 테스트 코드를 작성 및 green을 확인 했습니다.

---
+++ src/test/java/com/my/bob/core/external/redis/RedisTest.java
@@ -0,0 +1,37 @@
+package com.my.bob.core.external.redis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SpringBootTest
+@DisplayName("Redis 서비스 테스트")
+class RedisTest {
+
+    @Autowired
+    RedisTemplate<String, String> redisTemplate;
+
+    @Test
+    @DisplayName("Redis 기본 테스트(set, get)")
+    void redisTest() {
+        // given
+        String testKey = UUID.randomUUID().toString();
+        String testValue = "hello redis";
+
+        // when
+        redisTemplate.opsForValue().set(testKey, testValue);
+        String getValue = redisTemplate.opsForValue().get(testKey);
+
+        // then
+        assertThat(getValue).isEqualTo(testValue);
+        redisTemplate.delete(testKey);
+    }
+}
